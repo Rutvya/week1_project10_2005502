840cacb7351f709a84083204880058bc223fc256
Rutvya <rutvij.joshi@yahoo.com>
2020-12-09 15:40:57 +0530

    Datasheet added

--- problem10.m
+++ problem10.m
@@ -6,6 +6,9 @@ l=length(V);
 for i=1:l
     if V(i,1)>Vz
         I(i,1)=Is*(exp(V(i,1)/(0.02527*2))-1);
+        if I(i,1)>1
+            I(i,1)=1;
+        end
     else
         I(i,1)=-Inf;
     end
